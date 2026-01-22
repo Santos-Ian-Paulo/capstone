45f78fda7f1cfc9ba70138ba1b9ed7503bfd82cf
Ian Paulo Santos <iansantosgamingacc@gmail.com>
2026-01-22 12:27:46 +0800

    Add files via upload
    validation code for inserting documents int NoSQL

---
+++ capstone_repository.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+from datetime import datetime
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["capstone_repository"]
+collection = db("capstones")
+
+def validate_capstone(doc):
+    required_fields = [
+        "title",
+        "abstract",
+        "specialization",
+        "year",
+        "keywords",
+        "recommendations_text",
+        "summary_conclusion",
+        "vectors"
+    ]
+
+    for field in required_fields:
+        if field not in doc:
+            raise ValueError(f"Missing field: {field}")
+        
+    if doc["specialization"] not in ["web", "database", "network"]:
+        raise ValueError("Invalid specialization")
